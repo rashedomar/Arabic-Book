5f339e2873007ecbc01067fd349422deae1e59f5
rashed <rashedkoso90@gmail.com>
2019-11-24 15:31:16 +0200

    Add files via upload

---
+++ config.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+
+    //host
+    'db_host' => 'localhost',
+
+    //database username
+    'db_user' => '',
+
+    //database password
+    'db_pass' => '',
+
+    //database name
+    'db_name' => '',
+
+];
---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * @author                   Rakan Salem <i.rakkan.77@gmail.com>
+ * @copyright            (c) 2017, Rakan Salem. All Rights Reserved.
+ */
+
+require 'public'.DIRECTORY_SEPARATOR.'index.php';
