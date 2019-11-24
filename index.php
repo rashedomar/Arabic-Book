b157f87d303fb06de96ec8d13a3f8f75765f0b71
Rashed M Omar <27584247+rashedomar@users.noreply.github.com>
2019-11-24 16:32:17 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,4 @@
 <?php
-/**
- * @author                   Rakan Salem <i.rakkan.77@gmail.com>
- * @copyright            (c) 2017, Rakan Salem. All Rights Reserved.
- */
+
 
 require 'public'.DIRECTORY_SEPARATOR.'index.php';
